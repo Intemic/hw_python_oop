f4cede96858b704b45c7b5b2f7f7c6f4adcd3b56
Антон Лучик <Intemic@yandex.ru>
2023-01-10 22:27:48 +0500

    Устранение замечаний после проверки

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, asdict
+from typing import Type
 
 
 @dataclass
@@ -147,7 +148,7 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    TYPE_TRAINING: dict[str, type[Training]] = {
+    TYPE_TRAINING: dict[str, Type[Training]] = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
